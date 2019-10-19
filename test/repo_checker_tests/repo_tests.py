5cebfc7f6824852d0615742e485ede33829bd850
xzhang2437 <xzhang2437-c@my.cityu.edu.hk>
2019-10-19 19:38:41 +0800

    update test repo

--- test/repo_checker_tests/repo_tests.py
+++ test/repo_checker_tests/repo_tests.py
@@ -8,7 +8,7 @@ from checker.repo_checker import repo
 
 CURRENT = p.dirname(p.dirname(p.abspath(__file__)))
 OUTPUT = p.join(CURRENT, 'resources/tmp')
-REPO_PATH = p.join(OUTPUT, 'repo_for_test/')
+REPO_PATH = p.join(OUTPUT, 'test_repo/')
 
 
 class GitTests(unittest.TestCase):
@@ -16,7 +16,7 @@ class GitTests(unittest.TestCase):
     def setUp(self):
         if not os.path.exists(OUTPUT):
             os.makedirs(OUTPUT)
-        test_repo_resource = zipfile.ZipFile(p.join(CURRENT, 'resources/repo_for_test.zip'), 'r')
+        test_repo_resource = zipfile.ZipFile(p.join(CURRENT, 'resources/test_repo.zip'), 'r')
         for file in test_repo_resource.namelist():
             test_repo_resource.extract(file, OUTPUT)
         self.test_repo = repo.GitRepo(REPO_PATH)
@@ -25,7 +25,7 @@ class GitTests(unittest.TestCase):
         shutil.rmtree(OUTPUT, ignore_errors=True)
 
     def test_commit_info(self):
-        right_result = [['2019-10-07', 5, 10], ['2019-09-30', 2, 4]]
+        right_result = [['2019-10-19', 7, 13]]
         self.assertEqual(right_result, self.test_repo.commit_info())
 
     def test_last_day(self):
