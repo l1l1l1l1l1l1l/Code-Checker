0c6054541d6d316a8e3834a9f29e00506d65c26b
ChuoSky <ztfzhai@163.com>
2019-10-29 13:40:59 +0800

    Update complexity.py
    Optimize the score calculation method and change the demonstration.

--- checker/complexityChecker/complexity.py
+++ checker/complexityChecker/complexity.py
@@ -1,34 +1,66 @@
 import os
 
+#def of colors
+PASS = '\033[0;32m'
+#NOTICE = '\033[0;33;40m'
+NOTICE = '\033[0;33m'
+ALERT = '\033[0;31m'
+ENDSIG = '\033[0m'
+
 #example of dir = 'd:\mccabe'
 def flake8(dir):
-    command = 'flake8 --max-complexity 6 ' + dir
+    command = 'flake8 --max-complexity 1 ' + dir
     result = os.popen(command)
     info = result.readlines()
     score = 100
+
+    #count of different results
+    countAlert = 0
+    countNotice9 = 0
+    countNotice8 = 0
+    countNotice7 = 0
+    countNotice6 = 0
+    countPass = 0
+
     for line in info:
-        print(line)
         complexity = int(line[-3:-2])
-        if(score != 0 and complexity >=10):
-            score = 0
-        elif(score > 90 and complexity == 6):
-            if (score - 3 > 90):
-                score = score - 3
-            else:
-                score = 90
-        elif(score > 80 and complexity == 7):
-            if (score - 3 > 80):
-                score = score - 3
-            else:
-                score = 80
-        elif(score > 70 and complexity == 8):
-            if (score - 3 > 70):
-                score = score - 3
-            else:
-                score = 70
-        elif (score > 60 and complexity == 9):
-            if (score - 3 > 60):
-                score = score - 3
-            else:
-                score = 60
-    return score
+
+        #count different types of complexity
+        if(complexity >= 10):
+            countAlert = countAlert + 1
+            print(ALERT + 'ALERT(' + str(countAlert) + ')')
+            print('\t' + line + ENDSIG)
+        elif(complexity < 6):
+            countPass = countPass + 1
+        else:
+            if(complexity == 9):
+                countNotice9 = countNotice9 + 1
+            if (complexity == 8):
+                countNotice8 = countNotice8 + 1
+            if (complexity == 7):
+                countNotice7 = countNotice7 + 1
+            if (complexity == 6):
+                countNotice6 = countNotice6 + 1
+            printNotice(line, countNotice6, countNotice7, countNotice8, countNotice9)
+
+    # print result
+    print(ALERT + 'ALERT(' + str(countAlert) + ')' + ENDSIG + '\t' +
+          NOTICE + 'NOTICE(' + countNotice(countNotice6, countNotice7, countNotice8,countNotice9) + ')' + ENDSIG + '\t' +
+          PASS + 'PASS(' + str(countPass) + ')' + ENDSIG + '\t')
+
+    # calculate score
+    if (countAlert > 0):
+        score = 0
+    else:
+        score = 1 - ((0.50 * countNotice6 + 0.70 * countNotice7 + 0.85 * countNotice8 + 1 * countNotice9) /
+                     (countNotice6 + countNotice7 + countNotice8 + countNotice9 + countPass))
+        score = score * 100
+    return int(score)
+
+def countNotice(count6, count7, count8, count9):
+    count = count6 + count7 + count8 + count9
+    return str(count)
+
+def printNotice(line, count6, count7, count8, count9):
+    print(NOTICE + 'NOTICE(' + countNotice(count6, count7, count8, count9) + ')')
+    print('\t' + line + ENDSIG)
