ba9105cc855d43c7065e7197b86f073ac3261f3d
xzhang2437 <xzhang2437-c@my.cityu.edu.hk>
2019-11-20 15:47:53 +0800

    update coverage checker

--- checker/coverage_checker/cov.py
+++ checker/coverage_checker/cov.py
@@ -1,18 +1,29 @@
+import unittest
+import coverage
 import os.path as p
-import subprocess
 
 
 class Coverage:
     def __init__(self, repo_path: str = './'):
         self.repo_path = p.abspath(repo_path)
 
-    def coverage_rate(self) -> int:
-        rate = subprocess.getoutput(f'{p.dirname(p.abspath(__file__))}/cov.sh {self.repo_path}')
-        try:
-            return int(rate)
-        except ValueError:
-            return int(-1)
+    def coverage_rate(self, output_path: str = './coverage_details') -> int:
+        loader = unittest.TestLoader()
+        start_dir = self.repo_path
+        suite = loader.discover(start_dir, pattern='*_tests.py')
+
+        runner = unittest.TextTestRunner()
+
+        cov = coverage.coverage()
+        cov.start()
+
+        runner.run(suite)
+
+        cov.stop()
+        cov.save()
+
+        return cov.html_report(directory=output_path)
 
 
 if __name__ == '__main__':
-    print(Coverage('/Users/xinyuzhang/Vivian/CityU/SoftwareEngineering/Project7/Codes/repo_for_test').coverage_rate())
+    print(Coverage(f'{p.dirname(p.abspath(__file__))}/../../').coverage_rate())
