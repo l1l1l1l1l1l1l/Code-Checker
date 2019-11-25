34c4916b86cb6ec53e55335ef71f1f1d5591e8e8
Diyuan Shi <diyuan@Tp-Yoga-2nd.me>
2019-11-25 22:44:38 +0800

    fix test of vulns and add detailed output of bandit

--- test/vulns_checker_tests/vulns_tests.py
+++ test/vulns_checker_tests/vulns_tests.py
@@ -8,12 +8,10 @@ import os.path as p
 # from checker.cli import PARSER
 from checker.vulns_checker.vulns import Check
 
-RESOURCES = p.join(p.dirname(p.dirname(p.abspath(__file__))), 'resources/vulns/')
+RESOURCES = p.join(
+    p.dirname(p.dirname(p.abspath(__file__))), 'resources/vulns/')
 OUTPUT = p.join(RESOURCES, 'tmp/pydo/')
 INPUTFILE = p.join(RESOURCES, 'pydo.tgz')
-
-TESTRESULT = RESOURCES + 'test.html'
-TRUERESULT = RESOURCES + 'true.html'
 
 
 class VulnsTest(unittest.TestCase):
@@ -23,17 +21,9 @@ class VulnsTest(unittest.TestCase):
         os.makedirs(OUTPUT)
         pydoFiles = tarfile.open(INPUTFILE, "r:gz")
         pydoFiles.extractall(path=OUTPUT)
-        Check(OUTPUT, 12722)
-
 
     def tearDown(self):
         shutil.rmtree(OUTPUT)
-        os.remove(TESTRESULT)
 
     def testResult(self):
-        if not os.path.isfile(TESTRESULT):
-            self.fail("no output result file found")
-        self.assertTrue(filecmp.cmp(TESTRESULT,TRUERESULT))
-
-
-
+        self.assertEqual(Check(OUTPUT, 12722), 41)
